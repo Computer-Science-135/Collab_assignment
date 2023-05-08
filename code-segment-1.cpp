716aee97520687540341ca062ef412b3ddee94e9
James Pristas <jpristas@bellagio.csn.edu>
2023-05-08 11:34:31 -0700

    The code segment

---
+++ code-segment-1.cpp
@@ -0,0 +1,27 @@
+/// @file code-segment-1.cpp
+/// @author
+/// @date
+/// @brief
+/// @note Read in the file and find the easter egg and print it out.
+
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <complex>
+#include <cctype>
+#include <string>
+
+
+using namespace std;
+
+
+
+int main() {
+
+
+
+
+return (0);
+
+}
