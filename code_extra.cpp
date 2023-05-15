6541d4f43861f4c334a807fe319ed02add381e17
Alan Manuel Oliva <a5005926206@bellagio.csn.edu>
2023-05-14 20:05:15 -0700

    The program is now written to display the EXTRA message in the hidden.txt file.

--- code_extra.cpp
+++ code_extra.cpp
@@ -1,7 +1,13 @@
-/// @file code_extra.cpp
-/// @author
-/// @date
-/// @brief
+/// @file  code_extra.cpp
+/// @author  Alan Oliva
+/// @date    05/14/2023
+/// @note I pledge my word of honor that I have abided by the
+/// CSN Academic Integrity Policy while completing this assignment.
+/// @brief   The program reads in a file and iterate each
+///          character and if it is not a number it will
+///          create and concatenate the found character to
+///          reveal the message.
+/// @note    10 minutes
 /// @note Read in the file and find the the word EXTRA
 
 
@@ -19,8 +25,29 @@ using namespace std;
 
 int main() {
 
+ // Declare Variables //
+
+    char c;      // Reads each character of file
+    string msg;  // Holds the Result of Message
 
 
+       // Create File Stream //
+    ifstream infile;
+    infile.open("hidden.txt");
+
+       // Loop each character found in the file
+       // and if it is not a number, it will create
+       // the message in a string one by one character.
+    while (!infile.eof()) {
+        infile >> c;
+        if (isdigit(c) != 0) {
+            msg += c;
+        }
+    }
+    infile.close();
+
+       // Display Message
+    cout << endl << msg << endl << endl;
 
 return (0);
 
